91d911468375b22ac3d682a5035d521ee135d6e2
Arifin Mozumder <100036799+ProgramingWithArifin@users.noreply.github.com>
2026-02-24 07:30:42 +0600

    toggling button done

--- script/script.js
+++ script/script.js
@@ -1,8 +1,29 @@
+// calling element 
 let totalNumber = document.getElementById("total-count");
 let interviewNumber = document.getElementById("interview-count");
 let rejectedNumber = document.getElementById("rejected-count");
-let allCard = document.getElementById("all-card")
+// calling containers 
+let allCard = document.getElementById("all-card");
+let mainContainer = document.querySelector("main");
+// calling buttons
+let allBtn = document.getElementById("all");
+let interviewBtn = document.getElementById("interview");
+let rejectedBtn = document.getElementById("rejected");
+// array of the interview and rejected tab 
+let interviewList = []
+let rejectedList =[]
+
 function count(){
-    totalNumber.innerText=allCard.children.length
-}
+    totalNumber.innerText=allCard.children.length;
+    interviewNumber.innerText=interviewList.length;
+    rejectedNumber.innerText= rejectedList.length;}
 count()
+
+function toggleStyle(id){
+    // removing style from all button 
+    allBtn.classList.remove('btn-info','text-white')
+    interviewBtn.classList.remove('btn-info','text-white')
+    rejectedBtn.classList.remove('btn-info','text-white')
+    let selectedBtn = document.getElementById(id)
+    selectedBtn.classList.add('btn-info','text-white')
+}
